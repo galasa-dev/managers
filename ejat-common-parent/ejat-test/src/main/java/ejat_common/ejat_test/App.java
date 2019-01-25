91f975144e4627a9406ed539784f9c7749323189
Bruce Abbott <Bruce.Abbott@ibm.com>
2019-01-25 10:59:29 +0000

    Add test to increase code coverage

--- ejat-common-parent/ejat-test/src/main/java/ejat_common/ejat_test/App.java
+++ ejat-common-parent/ejat-test/src/main/java/ejat_common/ejat_test/App.java
@@ -2,7 +2,19 @@ package ejat_common.ejat_test;
 
 public class App 
 {
-    public static String testMethod() {
+    public static String testMethod1() {
+    	return "test string";
+    }
+    
+    public static String testMethod2() {
+    	return "test string";
+    }
+    
+    public static String testMethod3() {
+    	return "test string";
+    }
+    
+    public static String testMethod4() {
     	return "test string";
     }
 }
--- ejat-common-parent/ejat-test/src/test/java/ejat_common/ejat_test/AppTest.java
+++ ejat-common-parent/ejat-test/src/test/java/ejat_common/ejat_test/AppTest.java
@@ -13,7 +13,22 @@ public class AppTest {
 	
 	@Test
 	public void test2() {
-		assertEquals(App.testMethod(), "test string");
+		assertEquals(App.testMethod1(), "test string");
+	}
+	
+	@Test
+	public void test3() {
+		assertEquals(App.testMethod2(), "test string");
+	}
+	
+	@Test
+	public void test4() {
+		assertEquals(App.testMethod3(), "test string");
+	}
+	
+	@Test
+	public void test5() {
+		assertEquals(App.testMethod4(), "test string");
 	}
 
 }
