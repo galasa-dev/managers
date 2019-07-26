c427a6a93c43b7a3b93afb4701973286e623a0f4
Richard Somers <richard.somers@ibm.com@richardss-mbp.hursley.uk.ibm.com>
2019-07-26 15:00:20 +0100

    Increased Enum values

--- voras-common-parent/dev.voras.common.zos3270.manager/src/main/java/dev/voras/common/zos3270/AttentionIdentification.java
+++ voras-common-parent/dev.voras.common.zos3270.manager/src/main/java/dev/voras/common/zos3270/AttentionIdentification.java
@@ -5,7 +5,14 @@ public enum AttentionIdentification {
 	ENTER((byte)0x7d),
 	CLEAR((byte)0x6d),
 	PF1((byte)0xf1),
+	PF2((byte)0xf2),
 	PF3((byte)0xf3),
+	PF4((byte)0xf4),
+	PF5((byte)0xf5),
+	PF6((byte)0xf6),
+	PF7((byte)0xf7),
+	PF8((byte)0xf8),
+	PF9((byte)0xf9),
 	STRUCTURED_FIELD((byte)0x88);
 	
 	private final byte keyValue;
