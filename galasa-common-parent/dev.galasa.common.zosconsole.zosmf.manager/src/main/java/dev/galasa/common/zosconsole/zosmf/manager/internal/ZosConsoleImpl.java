b91e31f32f152ced582f842172e76f1bfde66c2c
David Roberts <david.roberts@uk.ibm.com>
2019-08-28 13:27:58 +0100

    zOS Console manager - phase 1

--- galasa-common-parent/dev.galasa.common.zosconsole.zosmf.manager/src/main/java/dev/galasa/common/zosconsole/zosmf/manager/internal/ZosConsoleImpl.java
+++ galasa-common-parent/dev.galasa.common.zosconsole.zosmf.manager/src/main/java/dev/galasa/common/zosconsole/zosmf/manager/internal/ZosConsoleImpl.java
@@ -10,15 +10,18 @@ import dev.galasa.common.zos.IZosImage;
 public class ZosConsoleImpl implements IZosConsole {
 	
 	private IZosImage image;
+	private String command;
 
 	@Override
 	public @NotNull IZosConsoleResponse issueCommand(@NotNull String command, @NotNull IZosImage image) throws ZosConsoleException {
+		this.command = command;
 		this.image = image;
-		return null;
+		IZosConsoleResponse zosConsoleResponse = (IZosConsoleResponse) new ZosConsoleResponseImpl();
+		return zosConsoleResponse;
 	}
 
 	@Override
 	public String toString() {
-		return this.image.getImageID();
+		return "COMMAND=" + this.command + (this.image != null ? " IMAGE=" +  this.image.getImageID() : "");
 	}
 }
