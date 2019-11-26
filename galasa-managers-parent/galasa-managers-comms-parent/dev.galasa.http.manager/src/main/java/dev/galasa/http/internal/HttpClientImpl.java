4540b2a24232d5d6d36d4527f0497ddaa497f2f2
James Davies <james.davies@ibm.com>
2019-11-26 10:03:06 +0000

    Simple auth fix allowing for auth headers to be set

--- galasa-managers-parent/galasa-managers-comms-parent/dev.galasa.http.manager/src/main/java/dev/galasa/http/internal/HttpClientImpl.java
+++ galasa-managers-parent/galasa-managers-comms-parent/dev.galasa.http.manager/src/main/java/dev/galasa/http/internal/HttpClientImpl.java
@@ -271,6 +271,7 @@ public class HttpClientImpl implements IHttpClient {
     public IHttpClient setAuthorisation(String username, String password) {
         credentialsProvider.clear();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+        enableAuthCache();
 
         return this;
     }
@@ -286,6 +287,7 @@ public class HttpClientImpl implements IHttpClient {
     public IHttpClient setAuthorisation(String username, String password, URI scope) {
         credentialsProvider.setCredentials(new AuthScope(scope.getHost(), scope.getPort()),
                 new UsernamePasswordCredentials(username, password));
+        enableAuthCache();
 
         return this;
     }
