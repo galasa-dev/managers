9ea22887d0d09857df9b24fec80ca7273d24eb6b
Clive Harris <clive.harris2@ibm.com>
2020-01-30 13:31:26 +0000

    Edited ElasticLog Manager information
    Updated package-info.java

--- galasa-managers-parent/galasa-managers-logging-parent/dev.galasa.elasticlog.manager/src/main/java/dev/galasa/elasticlog/internal/package-info.java
+++ galasa-managers-parent/galasa-managers-logging-parent/dev.galasa.elasticlog.manager/src/main/java/dev/galasa/elasticlog/internal/package-info.java
@@ -3,39 +3,38 @@
  * 
  * @galasa.manager ElasticLog
  * 
- * @galasa.release.state ALPHA - This Manager has been written to provide a way of exporting test data to ElasticSearch.  It has the bare 
+ * @galasa.release.state ALPHA - This Manager provides a way to export test data to ElasticSearch.  It contains the bare 
  * minimum code necessary to do that.  It has not been extensively tested. 
- * It is available for Galasa administrators to utilise to experiment with Elasic/Kabana dashboards
+ * It is available for Galasa administrators to experiment with Elasic/Kabana dashboards
  * 
  * @galasa.description
  * 
- * This manager is used to export test results to an elastic search endpoint, where the data can be visualised on a Kabana dashboard. 
- * Other managers can contribute to the information being exported to Elastic.
+ * This Manager is used to export test results to an elastic search endpoint, where the data can be visualised on a Kabana dashboard. 
+ * Other Managers can contribute to the information being exported to Elastic.
  * <br><br>
  * As an absolute minimum, the CPS properties <code>elasticlog.endpoint.address</code> and <code>elasticlog.endpoint.index</code> 
- * must be provided. By default, this manager will only log automated tests, to enable logging from locally run tests, 
- * <code>elasticlog.local.run.log</code> must be set to true.
+ * must be provided. By default, this Manager only logs automated tests. To enable logging from locally run tests, 
+ * <code>elasticlog.local.run.log</code> must be set to <code>true</code>.
  * <br><br>
- * This Manager will provide two ElasticSearch indexes. One of all test data, and one of the latest run for each test case and each 
- * testing environment.
+ * This Manager provides two ElasticSearch indexes. One of all test data, and one of the latest run for each test case and each testing environment.
  * 
  * @galasa.limitations
  * 
  * The Manager logs the following test information:-<br>
- * testCase<br>
- * runId<br>
- * startTimestamp<br>
- * endTimestamp<br>
- * requestor<br>
- * result<br>
- * testTooling<br>
- * testType<br>
- * testingEnvironment<br>
- * productRelease<br>
- * buildLevel<br>
- * customBuild<br>
- * testingAreas<br>
- * tags<br>
+ * - testCase<br>
+ * - runId<br>
+ * - startTimestamp<br>
+ * - endTimestamp<br>
+ * - requestor<br>
+ * - result<br>
+ * - testTooling<br>
+ * - testType<br>
+ * - testingEnvironment<br>
+ * - productRelease<br>
+ * - buildLevel<br>
+ * - customBuild<br>
+ * - testingAreas<br>
+ * - tags<br>
  * <br>
  * If additional testing information is required, please raise an Issue.
  */
