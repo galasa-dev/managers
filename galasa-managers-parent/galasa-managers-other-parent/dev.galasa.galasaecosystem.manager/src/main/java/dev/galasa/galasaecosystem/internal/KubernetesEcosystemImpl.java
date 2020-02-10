1733150b32d744c5a76f8adbfa5ca2ae83112fee
Michael Baylis <Michael.Baylis@uk.ibm.com>
2020-02-10 15:29:50 +0000

    Release 0.4.0

--- galasa-managers-parent/galasa-managers-other-parent/dev.galasa.galasaecosystem.manager/src/main/java/dev/galasa/galasaecosystem/internal/KubernetesEcosystemImpl.java
+++ galasa-managers-parent/galasa-managers-other-parent/dev.galasa.galasaecosystem.manager/src/main/java/dev/galasa/galasaecosystem/internal/KubernetesEcosystemImpl.java
@@ -74,7 +74,7 @@ public class KubernetesEcosystemImpl implements IKubernetesEcosystem {
 
     private final Yaml                       yaml = new Yaml();
 
-    private final String                     targetVersion = "0.4.0-SNAPSHOT";
+    private final String                     targetVersion = "0.4.0";
     private final HashMap<String, String>    yamlReplacements = new HashMap<>();
 
     private IHttpClient                      etcdHttpClient;
