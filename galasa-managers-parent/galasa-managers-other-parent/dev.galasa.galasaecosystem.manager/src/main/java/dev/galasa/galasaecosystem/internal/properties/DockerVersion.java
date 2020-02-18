5b661476c5b020104f71dbf894454c8639e16fed
Michael Baylis <Michael.Baylis@uk.ibm.com>
2020-02-18 08:51:58 +0000

    Updates for integrated testing

---
+++ galasa-managers-parent/galasa-managers-other-parent/dev.galasa.galasaecosystem.manager/src/main/java/dev/galasa/galasaecosystem/internal/properties/DockerVersion.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2020.
+ */
+package dev.galasa.galasaecosystem.internal.properties;
+
+import dev.galasa.framework.spi.ConfigurationPropertyStoreException;
+import dev.galasa.framework.spi.cps.CpsProperties;
+import dev.galasa.galasaecosystem.GalasaEcosystemManagerException;
+
+
+/**
+ * Docker Image Version
+ * 
+ * @galasa.cps.property
+ * 
+ * @galasa.name galasaecosystem.docker.version
+ * 
+ * @galasa.description The versions of the Docker images to be used with the Ecosystem
+ * 
+ * @galasa.required Yes
+ * 
+ * @galasa.default The setting of galasaecosystem.maven.version
+ * 
+ * @galasa.valid_values A valid Docker version literial
+ * 
+ * @galasa.examples 
+ * <code>galasaecosystem.docker.version=0.4.0</code>
+ * 
+ */
+public class DockerVersion extends CpsProperties {
+
+    public static String get() throws GalasaEcosystemManagerException {
+        try {
+            String version = getStringNulled(GalasaEcosystemPropertiesSingleton.cps(), "docker", "version") ;
+            if (version == null) {
+                version = getStringNulled(GalasaEcosystemPropertiesSingleton.cps(), "maven", "version") ;
+            }
+            if (version == null) {
+                throw new GalasaEcosystemManagerException("Property galasaecosystem.docker.version is missing");
+            }
+            return version;
+        } catch (ConfigurationPropertyStoreException e) {
+            throw new GalasaEcosystemManagerException("Problem retrieving the docker artifact version", e);
+        }
+    }
+}
