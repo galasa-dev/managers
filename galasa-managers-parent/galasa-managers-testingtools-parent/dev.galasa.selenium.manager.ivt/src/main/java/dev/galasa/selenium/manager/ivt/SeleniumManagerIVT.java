defe9b403bf11ec9b7b84d627832ea8f96fdb348
Richard Somers <rsomers1@sheffield.ac.uk>
2020-03-20 12:55:04 +0000

    Selenium IVT

--- galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.selenium.manager.ivt/src/main/java/dev/galasa/selenium/manager/ivt/SeleniumManagerIVT.java
+++ galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.selenium.manager.ivt/src/main/java/dev/galasa/selenium/manager/ivt/SeleniumManagerIVT.java
@@ -1,7 +1,11 @@
 package dev.galasa.selenium.manager.ivt;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
 import org.apache.commons.logging.Log;
+import org.openqa.selenium.Alert;
 
 import dev.galasa.Test;
 import dev.galasa.core.manager.Logger;
@@ -20,11 +24,34 @@ public class SeleniumManagerIVT {
     public ISeleniumManager seleniumManager;
 
     @Test
-    public void navigateGalasaDev() throws SeleniumManagerException {
-        IWebPage page = seleniumManager.allocateWebPage("http://www.galasa.dev", "A");
+    public void sendingKeysAndClearingFields() throws SeleniumManagerException {
+        IWebPage page = seleniumManager.allocateWebPage("https://duckduckgo.com", "A");
+        assertThat(page.getTitle()).containsOnlyOnce("DuckDuckGo");
+        assertThat(page.findElementById("search_form_input_homepage").getAttribute("value")).isEmpty();
+        page.sendKeysToElementById("search_form_input_homepage", "galasa");
+        assertThat(page.findElementById("search_form_input_homepage").getAttribute("value")).isEqualTo("galasa");
+        page.clearElementById("search_form_input_homepage");
+        assertThat(page.findElementById("search_form_input_homepage").getAttribute("value")).isEmpty();
+    }
+
+    @Test
+    public void clickingFields() throws SeleniumManagerException {
+        IWebPage page = seleniumManager.allocateWebPage("https://duckduckgo.com", "B");
+        assertThat(page.getTitle()).containsOnlyOnce("DuckDuckGo");
+        page.clickElementByCssSelector("a.header__button--menu.js-side-menu-open");
+        page.clickElementByLinkText("Twitter");
+        assertThat(page.getTitle()).contains("DuckDuckGo (@DuckDuckGo)");
+    }
+
+    @Test
+    public void navigateGalasaGithub() throws SeleniumManagerException {
+        IWebPage page = seleniumManager.allocateWebPage("https://duckduckgo.com", "C");
         logger.info("Page Title: " + page.getTitle());
-        assertThat(page.getTitle()).containsOnlyOnce("Home | Galasa");
-        page.clickElementByPartialLinkText("docs");
-        assertThat(page.getTitle()).containsOnlyOnce("Introduction | Galasa");
+        assertThat(page.getTitle()).containsOnlyOnce("DuckDuckGo");
+        page.sendKeysToElementById("search_form_input_homepage", "galasa dev github");
+        page.clickElementById("search_button_homepage");
+        page.clickElementByLinkText("galasa · GitHub");
+        page.clickElementByPartialLinkText("People");
+        assertThat(page.findElementsByLinkText("rsomers1998")).isNotEmpty();
     }
 }
