449b9ccca6629cec0581f5a59fe9a5ec53d337a1
David Roberts <david.roberts@uk.ibm.com>
2020-03-26 15:13:56 +0000

    Jobs obtained from list jobs missing status

--- galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zosconsole.zosmf.manager/src/main/java/dev/galasa/zosconsole/zosmf/manager/internal/properties/package-info.java
+++ galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zosconsole.zosmf.manager/src/main/java/dev/galasa/zosconsole/zosmf/manager/internal/properties/package-info.java
@@ -6,9 +6,5 @@
 /**
  * zOS Console zOSMF Manager - CPS Properties
  * 
- * <p>
- * The following properties are used by the zOS Batch zOSMF Manager:<br>
- * {@code zosconsole.command.[imageid].request.retry} <br>
- * </p>
  */
 package dev.galasa.zosconsole.zosmf.manager.internal.properties;
