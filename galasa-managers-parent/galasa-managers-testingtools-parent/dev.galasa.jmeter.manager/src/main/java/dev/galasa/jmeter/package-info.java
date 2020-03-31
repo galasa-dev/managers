7b45ddc2d7c7274eb667856faf4154b80747ab7a
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2020-03-31 18:44:08 +0100

    jmeter updates

--- galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.jmeter.manager/src/main/java/dev/galasa/jmeter/package-info.java
+++ galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.jmeter.manager/src/main/java/dev/galasa/jmeter/package-info.java
@@ -12,17 +12,17 @@
  * 
  * @galasa.description
  * 
- * This manager enables the tester to perform his own JMeter tests through the Galasa Framework. The manager will provide a container from the Docker Manager that is able to execute JMeter scripts. (JMX files)
- * The test does not need to worry how this container is provided and how it is kept up, the test has access to all the JMeter generated files inside the container.
+ * This Manager enables a JMeter session to run inside a Docker Container. The JMeter Manager requests a container from the Docker Manager inside which the JMeter scripts, or JMX files can run.  
+ * The test can access all JMeter-generated files inside the container without worrying about how the container is provisioned, maintained or shut down at the end of test.
+ * By using a containerized environment, the test can benefit from the associated standards of scalability and uniformity.
  * <br><br>
- * The container for this execution is fully provided by the Docker Manager and is also properly shutdown once the tests come to an end.
- * By using a containerised environment for JMeter the test has access to a certain standard of scalability and uniformity.
+ * 
  * <br><br> 
- * The logfiles and generated CSV files can be accessed after the JMeter tests are performed and the container becomes available for interaction.
- * The manager allows as many JMeter sessions as you have available Docker container slots on your machine.
+ * The logfiles and generated CSV files can be accessed once the JMeter tests are complete and the container becomes available for interaction.
+ * The JMeter Manager allows as many JMeter sessions as you have available Docker container slots on your machine.
  * 
  * @galasa.limitations
  * 
- * The JMeter manager is not able for the JMeter tests to be run remotely away from the Galasa framework on a targethost.
+ * JMeter tests cannot be run remotely on a target host.
  */
 package dev.galasa.jmeter;
