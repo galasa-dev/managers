e2bfb9e10585aaeea49f59e41f94a8b90274d8f6
Michael Baylis <Michael.Baylis@uk.ibm.com>
2020-03-31 10:41:20 +0100

    Allow simple IBundlresources for the test class

---
+++ galasa-managers-parent/galasa-managers-core-parent/dev.galasa.artifact.manager/src/main/java/dev/galasa/artifact/BundleResources.java
@@ -0,0 +1,28 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2019.
+ */
+package dev.galasa.artifact;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import dev.galasa.artifact.internal.ArtifactManagerField;
+import dev.galasa.framework.spi.ValidAnnotatedFields;
+
+/**
+ * Obtain a {@link IBundleResources} object for this test class
+ *
+ * @author Michael Baylis
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD })
+@ValidAnnotatedFields({ IBundleResources.class })
+@ArtifactManagerField
+public @interface BundleResources {
+
+}
