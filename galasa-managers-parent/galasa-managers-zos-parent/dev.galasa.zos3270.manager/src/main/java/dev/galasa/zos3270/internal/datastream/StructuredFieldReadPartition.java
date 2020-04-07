96110288cf5c4ae834583f4e9053406aab15efc7
Jakub Loucky <jakub.loucky@broadcom.com>
2020-04-07 23:10:38 +0200

    Add support for Read Partition Type code = 3
    Signed-off-by: Jakub Loucky <jakub.loucky@broadcom.com>

--- galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zos3270.manager/src/main/java/dev/galasa/zos3270/internal/datastream/StructuredFieldReadPartition.java
+++ galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zos3270.manager/src/main/java/dev/galasa/zos3270/internal/datastream/StructuredFieldReadPartition.java
@@ -7,16 +7,27 @@ package dev.galasa.zos3270.internal.datastream;
 
 import dev.galasa.zos3270.spi.NetworkException;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class StructuredFieldReadPartition extends StructuredField {
 
-    public static final byte QUERY = 0x02;
+    public static final byte QUERY      = 0x02;
+    public static final byte QUERY_LIST = 0x03;
+
+    public static final byte REQTYP_LIST       = 0x00;
+    public static final byte REQTYP_EQUIVALENT = 0x40;
+    public static final byte REQTYP_ALL        = (byte) 0x80;
 
     public enum Type {
-        QUERY
+        QUERY,
+        QUERY_LIST
     }
 
-    private final int  pid;
-    private final Type type;
+    private final int       pid;
+    private final Type      type;
+    private final Byte      reqtyp;
+    private final Set<Byte> qcodes = new TreeSet<>();
 
     public StructuredFieldReadPartition(byte[] structuredFieldData) throws NetworkException {
         this.pid = structuredFieldData[1];
@@ -24,6 +35,17 @@ public class StructuredFieldReadPartition extends StructuredField {
         switch (structuredFieldData[2]) {
             case QUERY:
                 this.type = Type.QUERY;
+                this.reqtyp = null;
+                break;
+            case QUERY_LIST:
+                this.type = Type.QUERY_LIST;
+                this.reqtyp = structuredFieldData[3];
+                if (reqtyp != REQTYP_LIST && reqtyp != REQTYP_EQUIVALENT && reqtyp != REQTYP_ALL) {
+                    throw new NetworkException("Unsupported Read Partition Request Type code = " + reqtyp);
+                }
+                for (int i = 4; i < structuredFieldData.length; i++) {
+                    this.qcodes.add(structuredFieldData[i]);
+                }
                 break;
             default:
                 throw new NetworkException("Unsupported Read Partition Type code = " + structuredFieldData[2]);
@@ -38,4 +60,11 @@ public class StructuredFieldReadPartition extends StructuredField {
         return this.type;
     }
 
+    public Byte getRequestType() {
+        return this.reqtyp;
+    }
+
+    public Set<Byte> getQcodes() {
+        return qcodes;
+    }
 }
