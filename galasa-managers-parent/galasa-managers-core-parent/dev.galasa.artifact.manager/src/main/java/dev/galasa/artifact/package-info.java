52dcf5f9dbf5a17837e6844a6d609b6bf617dcff
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2020-04-15 14:30:40 +0100

    add javadoc link to Artifact Manager

--- galasa-managers-parent/galasa-managers-core-parent/dev.galasa.artifact.manager/src/main/java/dev/galasa/artifact/package-info.java
+++ galasa-managers-parent/galasa-managers-core-parent/dev.galasa.artifact.manager/src/main/java/dev/galasa/artifact/package-info.java
@@ -14,5 +14,8 @@
  * 
  * This Manager allows a test to access files that are stored within the same bundle as the test code itself.  Helper methods are provided to stream and substitute symbolics within the loaded file.
  * <br><br>
+ *
+ * You can view the <a href="https://javadoc.galasa.dev/dev/galasa/artifact/package-summary.html">Javadoc documentation for this Manager here</a>.
+ * <br><br>
  */
 package dev.galasa.artifact;
