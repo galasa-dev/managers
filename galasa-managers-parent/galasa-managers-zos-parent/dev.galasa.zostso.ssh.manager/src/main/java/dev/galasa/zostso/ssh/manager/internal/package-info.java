6cb327d5e2c974c4f1e0d8feaf9652b948ce14ae
David Roberts <david.roberts@uk.ibm.com>
2020-04-30 10:11:11 +0100

    UNIX and TSO Command managers should be Alpha
    Signed-off-by: David Roberts <david.roberts@uk.ibm.com>

--- galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zostso.ssh.manager/src/main/java/dev/galasa/zostso/ssh/manager/internal/package-info.java
+++ galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zostso.ssh.manager/src/main/java/dev/galasa/zostso/ssh/manager/internal/package-info.java
@@ -8,10 +8,12 @@
  * 
  * @galasa.manager zOS TSO Command SSH
  * 
- * @galasa.release.state BETA - This Manager is feature complete but may contain known or unknown bugs.
+ * @galasa.release.state ALPHA - This Manager is being actively developed. It is subject to change and has not been extensively tested.
  * 
  * @galasa.description
  * 
  * This Manager is the internal implementation of the zOS TSO Command Manager using SSH.
+ * <p>
+ * See the <a href="../zos-manager">zOS Manager</a> for details of the zOS TSO Command Manager Annotations.
  */
 package dev.galasa.zostso.ssh.manager.internal;
