8774ced8759ca4f9d0fdfb9c582601c3bdc3131e
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2020-05-01 14:48:48 +0100

    fix tso and unix mgrs
    Signed-off-by: Caroline S McNamara <caroline.mcnamara@uk.ibm.com>

--- galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zostso.ssh.manager/src/main/java/dev/galasa/zostso/ssh/manager/internal/package-info.java
+++ galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zostso.ssh.manager/src/main/java/dev/galasa/zostso/ssh/manager/internal/package-info.java
@@ -8,10 +8,13 @@
  * 
  * @galasa.manager zOS TSO Command SSH
  * 
- * @galasa.release.state BETA - This Manager is feature complete but may contain known or unknown bugs.
+ * @galasa.release.state ALPHA - This Manager is being actively developed. It is subject to change and has not been extensively tested.
  * 
  * @galasa.description
  * 
  * This Manager is the internal implementation of the zOS TSO Command Manager using SSH.
+ * <br>br> See the <a href="../zos-manager">zOS Manager</a> for details of the z/OS TSO annotations and 
+ * code snippets.<br><br> You can view the <a href="https://javadoc.galasa.dev/dev/galasa/zostso/package-summary.html">Javadoc 
+ * documentation for the Manager here</a>. <br><br>
  */
 package dev.galasa.zostso.ssh.manager.internal;
