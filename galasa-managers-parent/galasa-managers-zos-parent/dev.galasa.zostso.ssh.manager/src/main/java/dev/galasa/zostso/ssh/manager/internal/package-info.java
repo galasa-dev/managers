1992b112ffc83ee80ece1bdd1a95e343bcbb3f84
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2020-05-05 13:53:58 +0100

    tag error fix
    Signed-off-by: Caroline S McNamara <caroline.mcnamara@uk.ibm.com>

--- galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zostso.ssh.manager/src/main/java/dev/galasa/zostso/ssh/manager/internal/package-info.java
+++ galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zostso.ssh.manager/src/main/java/dev/galasa/zostso/ssh/manager/internal/package-info.java
@@ -12,8 +12,8 @@
  * 
  * @galasa.description
  * 
- * This Manager is the internal implementation of the zOS TSO Command Manager using SSH.
- * <br>br> See the <a href="../zos-manager">zOS Manager</a> for details of the z/OS TSO annotations and 
+ * This Manager is the internal implementation of the zOS TSO Command Manager using SSH.<br><br>
+ * See the <a href="../zos-manager">zOS Manager</a> for details of the z/OS TSO annotations and 
  * code snippets.<br><br> You can view the <a href="https://javadoc.galasa.dev/dev/galasa/zostso/package-summary.html">Javadoc 
  * documentation for the Manager here</a>. <br><br>
  */
