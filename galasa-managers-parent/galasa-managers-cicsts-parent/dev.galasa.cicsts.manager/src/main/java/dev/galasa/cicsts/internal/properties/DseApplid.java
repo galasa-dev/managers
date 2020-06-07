ccfe58676620a4a8e789c0a1076767007979db06
Michael Baylis <Michael.Baylis@uk.ibm.com>
2020-06-07 11:35:39 +0100

    Correct SonarQube issues
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

--- galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.manager/src/main/java/dev/galasa/cicsts/internal/properties/DseApplid.java
+++ galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.manager/src/main/java/dev/galasa/cicsts/internal/properties/DseApplid.java
@@ -9,6 +9,26 @@ import dev.galasa.cicsts.CicstsManagerException;
 import dev.galasa.framework.spi.ConfigurationPropertyStoreException;
 import dev.galasa.framework.spi.cps.CpsProperties;
 
+/**
+ * Developer Supplied Environment - CICS TS Region - Applid
+ * 
+ * @galasa.cps.property
+ * 
+ * @galasa.name cicsts.dse.tag.[TAG].applid
+ * 
+ * @galasa.description Provides the applid of the CICS TS region for the DSE provisioner.  The applid setting
+ * is mandatory for a DSE region.
+ * 
+ * @galasa.required Yes if you want a DSE region, otherwide not required
+ * 
+ * @galasa.default None
+ * 
+ * @galasa.valid_values A value VTAM applid
+ * 
+ * @galasa.examples 
+ * <code>cicsts.dse.tag.PRIMARY.applid=CICS1A</code><br>
+ *
+ */
 public class DseApplid extends CpsProperties {
 
     public static String get(String tag) throws CicstsManagerException {
--- galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.manager/src/main/java/dev/galasa/cicsts/spi/ICicstsManagerSpi.java
+++ galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.manager/src/main/java/dev/galasa/cicsts/spi/ICicstsManagerSpi.java
@@ -14,6 +14,6 @@ public interface ICicstsManagerSpi {
     void registerProvisioner(ICicsRegionProvisioner provisioner);
 
     @NotNull
-	List<ICicsRegionLogonProvider> getLogonProviders();
+    List<ICicsRegionLogonProvider> getLogonProviders();
 
 }
