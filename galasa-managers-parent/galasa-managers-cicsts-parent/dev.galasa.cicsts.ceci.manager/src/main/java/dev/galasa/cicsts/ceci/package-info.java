f7a89bf64dac57215e7f968b54b791f319e1758f
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2020-07-02 16:38:12 +0100

    update CECI Manager
    Signed-off-by: Caroline S McNamara <caroline.mcnamara@uk.ibm.com>

--- galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.ceci.manager/src/main/java/dev/galasa/cicsts/ceci/package-info.java
+++ galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.ceci.manager/src/main/java/dev/galasa/cicsts/ceci/package-info.java
@@ -12,8 +12,12 @@
  * 
  * @galasa.description
  * 
- * This manager allows Galasa tests to issue CICS/TS CECI commands.<br><br>
- *
+ * This Manager allows Galasa tests to issue CICS/TS CECI commands.<br><br>
+ * Use the command-level interpreter (CECI) Manager to request a CECI instance in a Galasa test, 
+ * issue basic CECI commands  which can be processed interactively on a 3270 screen and 
+ * retrieve results. You can also use this Manager to write data to a temporary storage 
+ * queue and retrieve a signed-on user id.
+ * <br><br>
  * You can view the <a href="https://javadoc.galasa.dev/dev/galasa/cicsts/ceci/package-summary.html">Javadoc documentation for the Manager here</a>.
  * <br><br>
  * 
