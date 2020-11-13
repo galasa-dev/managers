3d211091f6179f366e556643d1c95dfc103d5f08
CJMeek <up887397@myport.ac.uk>
2020-11-13 10:39:29 +0000

    added missing copyrights
    Signed-off-by: CJMeek <up887397@myport.ac.uk>

--- galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.cemt.manager.ivt/src/main/java/dev/galasa/cicsts/cemt/manager/ivt/CEMTManagerIVT.java
+++ galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.cemt.manager.ivt/src/main/java/dev/galasa/cicsts/cemt/manager/ivt/CEMTManagerIVT.java
@@ -1,3 +1,10 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2020.
+ */
+
+
 package dev.galasa.cicsts.cemt.manager.ivt;
 
 import static org.assertj.core.api.Assertions.assertThat;
