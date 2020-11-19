74cef058a3feb77c7832f7617e64f2194ecf9666
Michael Baylis <Michael.Baylis@uk.ibm.com>
2020-11-19 19:40:43 +0000

    reinstate the terminal
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

--- galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.manager/src/main/java/dev/galasa/cicsts/ICeda.java
+++ galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.manager/src/main/java/dev/galasa/cicsts/ICeda.java
@@ -7,19 +7,22 @@ package dev.galasa.cicsts;
 
 import javax.validation.constraints.NotNull;
 
+import dev.galasa.zos3270.ITerminal;
+
 public interface ICeda {
    
-   public void createResource(@NotNull String resourceType, 
-         @NotNull String resourceName, @NotNull String groupName, @NotNull String resourceParameters) throws CedaException;
-   
-   public void installGroup(@NotNull String groupName) throws CedaException;
-   
-   public void installResource(@NotNull String resourceType, 
-         @NotNull String resourceName, @NotNull String cedaGroup) throws CedaException;
-   
-   public void deleteGroup(@NotNull String groupName) throws CedaException;
-   
-   public void deleteResource(@NotNull String resourceType, 
-         @NotNull String resourceName, @NotNull String groupName) throws CedaException;
+    public void createResource(@NotNull ITerminal terminal, @NotNull String resourceType, 
+            @NotNull String resourceName, @NotNull String groupName, @NotNull String resourceParameters) throws CedaException;
+      
+      public void installGroup(@NotNull ITerminal terminal, @NotNull String groupName) throws CedaException;
+      
+      public void installResource(@NotNull ITerminal terminal, @NotNull String resourceType, 
+            @NotNull String resourceName, @NotNull String cedaGroup) throws CedaException;
+      
+      public void deleteGroup(@NotNull ITerminal terminal, @NotNull String groupName) throws CedaException;
+      
+      public void deleteResource(@NotNull ITerminal terminal, @NotNull String resourceType, 
+            @NotNull String resourceName, @NotNull String groupName) throws CedaException;
+
 
 }
