88ea474cbf3420cdc7c9b4bf1f18a5521255e1c0
CJMeek <up887397@myport.ac.uk>
2021-03-04 16:32:18 +0000

    Auth for elasticlog manager
    Signed-off-by: CJMeek <up887397@myport.ac.uk>

---
+++ galasa-managers-parent/galasa-managers-logging-parent/dev.galasa.elasticlog.manager/src/main/java/dev/galasa/elasticlog/internal/properties/ElasticLogCredentials.java
@@ -0,0 +1,21 @@
+package dev.galasa.elasticlog.internal.properties;
+
+import dev.galasa.elasticlog.internal.ElasticLogManagerException;
+import dev.galasa.framework.spi.ConfigurationPropertyStoreException;
+import dev.galasa.framework.spi.cps.CpsProperties;
+
+public class ElasticLogCredentials extends CpsProperties {
+   public static String get() throws ElasticLogManagerException{
+      try {
+         String credentialsKey = getStringNulled(ElasticLogPropertiesSingleton.cps(),"auth", "credentials");
+         
+         if(credentialsKey == null) {
+            return "ELASTIC";
+         }
+         
+         return credentialsKey;
+      }catch(ConfigurationPropertyStoreException e) {
+         throw new ElasticLogManagerException("Failed to access the CPS to find the credential");
+      }
+   }
+}
