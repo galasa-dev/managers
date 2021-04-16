cd36ee2a490fd1e91e25d8285cd0676f2fa1a02f
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-04-16 12:50:00 +0100

    Windows Manager TPI/SPI
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

---
+++ galasa-managers-parent/galasa-managers-windows-parent/dev.galasa.windows.manager/src/main/java/dev/galasa/windows/IWindowsImage.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2021.
+ */
+package dev.galasa.windows;
+
+import java.nio.file.Path;
+
+import javax.validation.constraints.NotNull;
+
+import dev.galasa.ICredentials;
+import dev.galasa.ipnetwork.ICommandShell;
+import dev.galasa.ipnetwork.IIpHost;
+
+/**
+ * <p>
+ * Represents a Windows Image .
+ * </p>
+ * 
+ * <p>
+ * Use a {@link WindowsImage} annotation to populate this field with
+ * </p>
+ * 
+ * @author Michael Baylis
+ *
+ */
+public interface IWindowsImage {
+
+    /**
+     * Get the name of the Windows Image
+     * 
+     * @return The image ID, never null
+     */
+    @NotNull
+    String getImageID();
+
+    /**
+     * Retrieve the IP Network Host details
+     * 
+     * @return
+     */
+    @NotNull
+    IIpHost getIpHost();
+
+    /**
+     * Retrieve the default credentials for the Image.
+     * 
+     * @return The default credentials - see
+     *         {@link dev.galasa.framework.spi.creds.ICredentials}
+     * @throws WindowsManagerException if the credentials are missing or there is a
+     *                               problem with the credentials store
+     */
+    @NotNull
+    ICredentials getDefaultCredentials() throws WindowsManagerException;
+
+    @NotNull
+    ICommandShell getCommandShell() throws WindowsManagerException;
+
+    @NotNull
+    Path getRoot() throws WindowsManagerException;
+
+    @NotNull
+    Path getHome() throws WindowsManagerException;
+
+    @NotNull
+    Path getTmp() throws WindowsManagerException;
+
+}
---
+++ galasa-managers-parent/galasa-managers-windows-parent/dev.galasa.windows.manager/src/main/java/dev/galasa/windows/spi/IWindowsProvisionedImage.java
@@ -0,0 +1,12 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2021.
+ */
+package dev.galasa.windows.spi;
+
+import dev.galasa.windows.IWindowsImage;
+
+public interface IWindowsProvisionedImage extends IWindowsImage {
+
+}
