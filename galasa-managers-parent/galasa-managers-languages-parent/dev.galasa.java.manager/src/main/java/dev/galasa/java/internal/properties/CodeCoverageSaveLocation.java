3d8da7e82ca84bcab724f4f4465a8b451b778527
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-05-14 12:15:56 +0100

    feature to save jacoco files to remote location
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

---
+++ galasa-managers-parent/galasa-managers-languages-parent/dev.galasa.java.manager/src/main/java/dev/galasa/java/internal/properties/CodeCoverageSaveLocation.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2021.
+ */
+package dev.galasa.java.internal.properties;
+
+import dev.galasa.framework.spi.ConfigurationPropertyStoreException;
+import dev.galasa.framework.spi.cps.CpsProperties;
+import dev.galasa.java.JavaManagerException;
+
+/**
+ * Java jacoco code coverage save locations
+ * 
+ * @galasa.cps.property
+ * 
+ * @galasa.name java.jacoco.save.location
+ * 
+ * @galasa.description Indicate where to save the exec files that were generated in testing
+ * 
+ * @galasa.required No
+ * 
+ * @galasa.default None
+ * 
+ * @galasa.valid_values a valid URL
+ * 
+ * @galasa.examples 
+ * <code>java.jacoco.agent.location=http://upload/codecoverage</code>
+ * 
+ */
+public class CodeCoverageSaveLocation extends CpsProperties {
+
+    public static String get() throws JavaManagerException {
+        
+        try {
+            return getStringNulled(JavaPropertiesSingleton.cps(), "jacoco.save", "location") ;
+        } catch (ConfigurationPropertyStoreException e) {
+            throw new JavaManagerException("Problem retrieving the jacoco save location", e);
+        }
+    }
+}
