0694977a900e6c453fbf7b21cf3a23f8d1e407e8
Diana Slepikaite <d.slepikaite@gmail.com>
2021-06-04 15:58:52 +0100

    core tests
    Signed-off-by: Diana Slepikaite <d.slepikaite@gmail.com>

--- galasa-managers-parent/galasa-managers-core-parent/dev.galasa.core.manager.ivt/src/main/java/dev/galasa/core/manager/ivt/CoreManagerIVT.java
+++ galasa-managers-parent/galasa-managers-core-parent/dev.galasa.core.manager.ivt/src/main/java/dev/galasa/core/manager/ivt/CoreManagerIVT.java
@@ -41,25 +41,20 @@ public class CoreManagerIVT {
     
     @Test
     public void checkLogger() throws Exception {
-        if (logger == null) {
-            throw new Exception("Logger field is null, should have been filled by the Core Manager");
-        }
+        assertThat(logger).isNotNull();
         logger.info("The Logger field has been correctly initialised");
     }
 
     @Test
     public void checkRunName() throws Exception {
-        if (runName == null || runName.trim().isEmpty()) {
-            throw new Exception("Run Name field is null, should have been filled by the Core Manager");
-        }
+        assertThat(runName).isNotNull();
+        assertThat(runName.trim().isEmpty()).isFalse();
         logger.info("The Run Name  field has been correctly initialised = '" + runName + "'");
     }
 
     @Test
     public void checkCoreManager() throws Exception {
-        if (coreManager == null) {
-            throw new Exception("Core Manager field is null, should have been filled by the Core Manager");
-        }
+        assertThat(coreManager).isNotNull();
         logger.info("The Core Manager field has been correctly initialised");
     }
 
