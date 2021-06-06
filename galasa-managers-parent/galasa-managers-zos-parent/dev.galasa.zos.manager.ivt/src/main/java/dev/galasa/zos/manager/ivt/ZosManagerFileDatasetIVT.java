000ffe883c8f209b0598af5554be5480ed176041
Will Yates <hobbit1983@gmail.com>
2021-06-06 14:35:02 +0100

    more zosFile IVT tests
    Signed-off-by: Will Yates <hobbit1983@gmail.com>

--- galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zos.manager.ivt/src/main/java/dev/galasa/zos/manager/ivt/ZosManagerFileDatasetIVT.java
+++ galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zos.manager.ivt/src/main/java/dev/galasa/zos/manager/ivt/ZosManagerFileDatasetIVT.java
@@ -12,7 +12,6 @@ import java.util.HashMap;
 
 import org.apache.commons.logging.Log;
 
-import dev.galasa.ICredentialsUsernamePassword;
 import dev.galasa.Test;
 import dev.galasa.artifact.BundleResources;
 import dev.galasa.artifact.IBundleResources;
@@ -24,6 +23,7 @@ import dev.galasa.zos.IZosImage;
 import dev.galasa.zos.ZosImage;
 import dev.galasa.zosbatch.IZosBatch;
 import dev.galasa.zosbatch.IZosBatchJob;
+import dev.galasa.zosbatch.IZosBatchJobOutputSpoolFile;
 import dev.galasa.zosbatch.ZosBatch;
 import dev.galasa.zosbatch.ZosBatchException;
 import dev.galasa.zosfile.IZosDataset;
@@ -62,7 +62,6 @@ public class ZosManagerFileDatasetIVT {
     @ZosUNIXCommand(imageTag = IMG_TAG)
     public IZosUNIXCommand zosUNIXCommand;
     
-    private String userName = new String();
     private String runName  = new String();
 
     @Test
@@ -74,23 +73,50 @@ public class ZosManagerFileDatasetIVT {
         assertThat(resources).isNotNull();
         assertThat(logger).isNotNull();
         assertThat(imagePrimary.getDefaultCredentials()).isNotNull();
-        userName = ((ICredentialsUsernamePassword)imagePrimary.getDefaultCredentials()).getUsername().toLowerCase();
         runName = coreManager.getRunName().toUpperCase();
     }
     
-   private boolean checkThatPDSExists(String dataset) throws TestBundleResourceException, IOException, ZosBatchException {
-	   HashMap<String,Object> parms = new HashMap<>();
-	   parms.put("DATASET", dataset);
-	   String jcl = resources.retrieveSkeletonFileAsString("/resources/jcl/PDSCheck.jcl", parms);
-	   IZosBatchJob job = batch.submitJob(jcl, null);
-	   job.setShouldArchive(true);
-	   job.setShouldCleanup(true);
-	   int rc = job.waitForJob();
-	   if(rc == 0)
-		   return true;
-	   else
-		   return false;
-   }
+    /**
+     * Run some JCL to check that a PDS has been created
+     * This is used to test that zosFile is working correctly
+     * @param dataset The name of the dataset that we want to test
+     * @return a boolean to signify that the dataset exists (or not)
+     * @throws TestBundleResourceException
+     * @throws IOException
+     * @throws ZosBatchException
+     */
+    private boolean checkThatPDSExists(String dataset) throws TestBundleResourceException, IOException, ZosBatchException {
+    	HashMap<String,Object> parms = new HashMap<>();
+    	parms.put("DATASET", dataset);
+    	String jcl = resources.retrieveSkeletonFileAsString("/resources/jcl/PDSCheck.jcl", parms);
+    	return(batch.submitJob(jcl, null).waitForJob() == 0);
+    }
+    
+    /**
+     * Run a piece of JCL to echo the content of the member in the PDS requested
+     * Check that the strings provided as content exist within the content of the PDS member
+     * @param pds The data set name 
+     * @param member the member within the dataset
+     * @param content lines of content to check
+     * @return a boolean true if the content exists in the pds member, false if something is missing or the member does not exist
+     * @throws ZosBatchException
+     * @throws TestBundleResourceException
+     * @throws IOException
+     */
+    private boolean checkContentWasWritten(String pds, String member, String... content) throws ZosBatchException, TestBundleResourceException, IOException {
+    	String fullName = pds+"("+member+")";
+    	HashMap<String, Object> parms = new HashMap<>();
+    	parms.put("MEMBER_NAME",fullName);
+    	IZosBatchJob job = batch.submitJob(resources.retrieveSkeletonFileAsString("/resources/jcl/list.jcl", parms),null);
+    	if(job.waitForJob() > 0)
+    		return false;
+    	IZosBatchJobOutputSpoolFile output = job.getSpoolFile("SYSUT2");
+    	for(String c : content) {
+    		if(!output.getRecords().contains(c))
+    			return false;
+    	}
+    	return true;
+    }
    
    @Test
    public void testExistingDS() throws ZosBatchException, TestBundleResourceException, IOException, ZosDatasetException {
@@ -105,14 +131,9 @@ public class ZosManagerFileDatasetIVT {
 	   assertThat(checkThatPDSExists(desiredDataSetName)).isFalse();
 	   assertThat(fileHandler.newDataset(desiredDataSetName, imagePrimary).exists()).isFalse();
    }
-    
-   @Test
-   public void testPDSCreate() throws Exception {
-	   String desiredDataSetName = "CTS.GALASA." + runName;
-	   assertThat(checkThatPDSExists(desiredDataSetName)).isFalse();
-	   logger.info("Checked that " + desiredDataSetName + " doesn't currently exist");
-	   
-	   IZosDataset ds = fileHandler.newDataset(desiredDataSetName, imagePrimary);
+   
+   private IZosDataset createBasicDataset(String name) throws ZosDatasetException {
+	   IZosDataset ds = fileHandler.newDataset(name, imagePrimary);
 	   ds.setDatasetOrganization(DatasetOrganization.SEQUENTIAL);
 	   ds.setRecordFormat(RecordFormat.FIXED_BLOCKED);
 	   ds.setRecordlength(80);
@@ -120,8 +141,19 @@ public class ZosManagerFileDatasetIVT {
 	   ds.setUnit("SYSDA");
 	   ds.setSpace(SpaceUnit.TRACKS, 1, 1);
 	   ds.create();
+	   return ds;
+   }
+    
+   @Test
+   public void testPDSCreate() throws Exception {
+	   String desiredDataSetName = "CTS.GALASA." + runName;
+	   assertThat(checkThatPDSExists(desiredDataSetName)).isFalse();
+	   logger.info("Checked that " + desiredDataSetName + " doesn't currently exist");
+	   
+	   IZosDataset ds = createBasicDataset(desiredDataSetName);
 	   
 	   assertThat(checkThatPDSExists(desiredDataSetName)).isTrue();
+	   assertThat(ds.exists()).isTrue();
 	   logger.info("Checked that " + desiredDataSetName + " now exists");
 	   
 	   ds.delete();
@@ -129,4 +161,48 @@ public class ZosManagerFileDatasetIVT {
 	   assertThat(checkThatPDSExists(desiredDataSetName)).isFalse();
 	   logger.info("Checked that " + desiredDataSetName + " has been deleted");
    }
+   
+   @Test
+   public void datasetAttributeCheck() throws ZosBatchException, TestBundleResourceException, IOException, ZosDatasetException {
+	   String desiredDataSetName = "CTS.GALASA." + runName;
+	   assertThat(checkThatPDSExists(desiredDataSetName)).isFalse();
+	   logger.info("Checked that " + desiredDataSetName + " doesn't currently exist");
+	   
+	   createBasicDataset(desiredDataSetName);
+	   
+	   IZosDataset ds = fileHandler.newDataset(desiredDataSetName, imagePrimary);
+	   assertThat(ds.exists()).isTrue();
+	   
+	   assertThat(ds.getDatasetOrganization()).isEqualTo(DatasetOrganization.SEQUENTIAL);
+	   assertThat(ds.getRecordFormat()).isEqualTo(RecordFormat.FIXED_BLOCKED);
+	   assertThat(ds.getRecordlength()).isEqualTo(80);
+	   assertThat(ds.getBlockSize()).isEqualTo(32780);
+	   assertThat(ds.getUnit()).isEqualTo("SYSDA");
+	   assertThat(ds.getSpaceUnit()).isEqualTo(SpaceUnit.TRACKS);
+	   
+   }
+   
+   @Test
+   public void testPDSMemberCreate() throws Exception {
+	   String desiredDataSetName = "CTS.GALASA." + runName;
+	   String memberName = "HOBBIT";
+	   String content = "Basic PDS Member test";
+	   String content2 = "a second line of content";
+	   IZosDataset ds = createBasicDataset(desiredDataSetName);
+	   ds.memberCreate(memberName);
+	   assertThat(ds.memberExists(memberName)).isTrue();
+	   
+	   ds.memberStoreText(memberName, content);
+	   ds.memberStoreText(memberName, content2);
+	   
+	   //check through JCL that we wrote what we thought we wrote
+	   assertThat(checkContentWasWritten(desiredDataSetName, memberName, content, content2));
+	   
+	   String result = ds.memberRetrieveAsText(memberName);
+	   assertThat(result).startsWith(content);
+	   assertThat(result).endsWith(content2);
+	   
+	   ds.memberDelete(memberName);
+	   assertThat(ds.memberExists(memberName)).isFalse();
+	   }
 }
