251505be08ae08473faa2d1f3d8a44abf9c49cf6
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-07-21 17:48:24 +0100

    redrive
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

--- galasa-managers-parent/galasa-managers-core-parent/dev.galasa.core.manager/src/main/java/dev/galasa/core/manager/CoreManager.java
+++ galasa-managers-parent/galasa-managers-core-parent/dev.galasa.core.manager/src/main/java/dev/galasa/core/manager/CoreManager.java
@@ -14,7 +14,7 @@ import java.lang.annotation.Target;
 import dev.galasa.core.manager.internal.CoreManagerField;
 import dev.galasa.framework.spi.ValidAnnotatedFields;
 
-/**  Test change2
+/**  Test change23
  * <p>
  * Fill this field with the Core Manager instance.
  * </p>
