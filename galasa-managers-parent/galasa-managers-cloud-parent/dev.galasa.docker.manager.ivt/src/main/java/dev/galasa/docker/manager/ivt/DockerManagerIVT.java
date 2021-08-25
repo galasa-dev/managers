86d442ab5a89dc8456c721565dc7153fc4e0b9b9
James Davies <jdavies47@hotmail.co.uk>
2021-08-25 09:47:03 +0100

    copyright fix
    Signed-off-by: James Davies <jdavies47@hotmail.co.uk>

--- galasa-managers-parent/galasa-managers-cloud-parent/dev.galasa.docker.manager.ivt/src/main/java/dev/galasa/docker/manager/ivt/DockerManagerIVT.java
+++ galasa-managers-parent/galasa-managers-cloud-parent/dev.galasa.docker.manager.ivt/src/main/java/dev/galasa/docker/manager/ivt/DockerManagerIVT.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- * 
- * (c) Copyright IBM Corp. 2020.
- */
+* Copyright contributors to the Galasa project 
+*/
 package dev.galasa.docker.manager.ivt;
 
 import static org.assertj.core.api.Assertions.assertThat;
--- galasa-managers-parent/galasa-managers-cloud-parent/dev.galasa.docker.manager/src/main/java/dev/galasa/docker/internal/IDockerImageBuilder.java
+++ galasa-managers-parent/galasa-managers-cloud-parent/dev.galasa.docker.manager/src/main/java/dev/galasa/docker/internal/IDockerImageBuilder.java
@@ -1,3 +1,6 @@
+/*
+* Copyright contributors to the Galasa project 
+*/
 package dev.galasa.docker.internal;
 
 import java.io.InputStream;
