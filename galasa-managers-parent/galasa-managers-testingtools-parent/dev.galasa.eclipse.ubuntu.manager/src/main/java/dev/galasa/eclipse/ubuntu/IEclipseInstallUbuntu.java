794a9f7b1c77a462190fb98549ba52321b9c2c62
Reece Williams <reecewilliams1105@gmail.com>
2021-08-26 15:38:19 +0100

    Fixed copyright issues
    Signed-off-by: Reece Williams <reecewilliams1105@gmail.com>

--- galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.eclipse.ubuntu.manager/src/main/java/dev/galasa/eclipse/ubuntu/IEclipseInstallUbuntu.java
+++ galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.eclipse.ubuntu.manager/src/main/java/dev/galasa/eclipse/ubuntu/IEclipseInstallUbuntu.java
@@ -1,8 +1,7 @@
 /*
- * Licensed Materials - Property of IBM
- * 
- * (c) Copyright IBM Corp. 2021.
- */
+* Copyright contributors to the Galasa project 
+*/
+
 package dev.galasa.eclipse.ubuntu;
 
 import dev.galasa.eclipse.IEclipseInstall;
--- galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.eclipse.windows.manager/src/main/java/dev/galasa/eclipse/windows/EclipseInstallWindows.java
+++ galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.eclipse.windows.manager/src/main/java/dev/galasa/eclipse/windows/EclipseInstallWindows.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- * 
- * (c) Copyright IBM Corp. 2021.
- */
+* Copyright contributors to the Galasa project 
+*/
 
 package dev.galasa.eclipse.windows;
 
