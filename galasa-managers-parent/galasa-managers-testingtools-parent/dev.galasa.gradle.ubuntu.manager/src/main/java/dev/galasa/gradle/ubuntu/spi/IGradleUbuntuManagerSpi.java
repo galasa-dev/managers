e88ab95e458053d937c223f776e4cb57f8f5258f
Matthew Chivers <matthewchivers@outlook.com>
2021-09-03 16:45:00 +0100

    Formatting and refactoring.
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

--- galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.gradle.ubuntu.manager/src/main/java/dev/galasa/gradle/ubuntu/spi/IGradleUbuntuManagerSpi.java
+++ galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.gradle.ubuntu.manager/src/main/java/dev/galasa/gradle/ubuntu/spi/IGradleUbuntuManagerSpi.java
@@ -2,7 +2,7 @@
  * Copyright contributors to the Galasa project
  */
 
-package dev.galasa.gradle.ubuntu;
+package dev.galasa.gradle.ubuntu.spi;
 
 /** 
  * Gradle Ubuntu Manager Service Provider Interface.
