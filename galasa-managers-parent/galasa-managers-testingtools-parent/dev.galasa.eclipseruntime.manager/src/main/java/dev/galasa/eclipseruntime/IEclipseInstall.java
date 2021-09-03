19f713ce049064149c244a3544eb78450c7c76ea
Reece Williams <reecewilliams1105@gmail.com>
2021-09-03 12:05:49 +0100

    Fix for naming issue again
    Signed-off-by: Reece Williams <reecewilliams1105@gmail.com>

---
+++ galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.eclipseruntime.manager/src/main/java/dev/galasa/eclipseruntime/IEclipseInstall.java
@@ -0,0 +1,37 @@
+/*
+* Copyright contributors to the Galasa project 
+*/
+
+package dev.galasa.eclipseruntime;
+
+/**
+ * 
+ * @author Reece Williams
+ *
+ */
+public interface IEclipseInstall {
+	
+	/**
+	 * Returns the version of eclipse being used
+	 * @return EclipseVersion
+	 */
+	EclipseVersion getEclipseVersion();
+	
+	/**
+	 * Returns the types of eclipse being used
+	 * @return EclipseType
+	 */
+	EclipseType getEclipseType();
+	
+	/**
+	 * Returns the Instance of java being used
+	 * @return String
+	 */
+	String getJavaInstallation();
+	
+	/**
+	 * Returns the location of the eclipse install
+	 * @return String
+	 */
+	String getEclipseInstallation();
+}
