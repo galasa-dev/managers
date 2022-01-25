d19a4e8f658318a4a3e85ab484d4e45a39bff65b
JansenGSY <keesjansen@Keess-MacBook-Pro.local>
2022-01-25 13:57:25 +0000

    Added response text, toStringing correct value in postString, and added license statement to new Exception code
    Signed-off-by: JansenGSY <keesjansen@Keess-MacBook-Pro.local>

--- galasa-managers-parent/galasa-managers-cloud-parent/dev.galasa.docker.manager/src/main/java/dev/galasa/docker/internal/DockerEngineImpl.java
+++ galasa-managers-parent/galasa-managers-cloud-parent/dev.galasa.docker.manager/src/main/java/dev/galasa/docker/internal/DockerEngineImpl.java
@@ -475,7 +475,7 @@ public class DockerEngineImpl implements IDockerEngine {
 			case HttpStatus.SC_NO_CONTENT:
 				return null;
 			case HttpStatus.SC_NOT_FOUND:
-				throw new DockerNotFoundException(response.toString());
+				throw new DockerNotFoundException("Docker engine not found: " + response.toString());
 			}
 
 			logger.error("Post failed to docker engine - " + response.getAsString());
@@ -510,7 +510,7 @@ public class DockerEngineImpl implements IDockerEngine {
 			case HttpStatus.SC_NO_CONTENT:
 				return null;
 			case HttpStatus.SC_NOT_FOUND:
-				throw new DockerNotFoundException(response.toString());
+				throw new DockerNotFoundException("Docker engine not found: " + resp.toString());
 			}
 
 			logger.error("Post failed to docker engine - " + resp);
