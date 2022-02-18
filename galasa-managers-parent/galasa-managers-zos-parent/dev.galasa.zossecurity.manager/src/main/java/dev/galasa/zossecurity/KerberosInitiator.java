d018059f58298b08702c5a9cd4c611fb09cae64c
Will Yates <hobbit1983@gmail.com>
2022-02-18 09:49:22 +0000

    remove 3x printStackTrace calls (#735)
    Signed-off-by: Will Yates <hobbit1983@gmail.com>

--- galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zossecurity.manager/src/main/java/dev/galasa/zossecurity/KerberosInitiator.java
+++ galasa-managers-parent/galasa-managers-zos-parent/dev.galasa.zossecurity.manager/src/main/java/dev/galasa/zossecurity/KerberosInitiator.java
@@ -266,8 +266,7 @@ public class KerberosInitiator {
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new ZosSecurityManagerException("Interrupted", e);
 		}
 
 		// decode
