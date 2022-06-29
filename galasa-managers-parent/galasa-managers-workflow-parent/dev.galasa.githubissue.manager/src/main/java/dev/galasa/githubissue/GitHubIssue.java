4c6539f082e5d787d411c6e6c3ec050a7aa8f983
Michael Baylis <Michael.Baylis@uk.ibm.com>
2022-06-29 17:36:33 +0100

    GitHubIssue Manager template
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

---
+++ galasa-managers-parent/galasa-managers-workflow-parent/dev.galasa.githubissue.manager/src/main/java/dev/galasa/githubissue/GitHubIssue.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright contributors to the Galasa project
+ */
+package dev.galasa.githubissue;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * This annotation will cause the Manager to influence the "failed" result of the test based on 
+ * whether a GitHub issue is open or closed.
+ * 
+ * @author Michael Baylis
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GitHubIssue {
+    
+    /**
+     * The issue to check against
+     */
+    public String issue();
+    
+    /**
+     * The repository to look for the issue, if not provided, will use the default from the CPS
+     */
+    public String repository() default "";
+
+    /**
+     * The CPS id of the GitHub instance, be it public GitHub or an enterprise instance
+     */
+    public String githubId() default "DEFAULT";
+    
+    
+    /**
+     * One or more REGEX strings to check in the failing exception, if any of them match, then this annotation is valid and therefore GitHub
+     * will be checked.   If no REGEX is provided, any failure is valid.
+     */
+    public String[] regex() default {};
+
+}
