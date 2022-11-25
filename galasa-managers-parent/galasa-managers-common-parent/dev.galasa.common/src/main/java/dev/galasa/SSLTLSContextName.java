491778bcc608c10de26bcdfdf91e33335f8d8ded
Savvas Kyriacou <savvasky@uk.ibm.com>
2022-11-25 14:24:22 +0000

    Updated HTTPClientImpl to use dev.galasa.common

---
+++ galasa-managers-parent/galasa-managers-common-parent/dev.galasa.common/src/main/java/dev/galasa/SSLTLSContextName.java
@@ -0,0 +1,32 @@
+package dev.galasa.common;
+
+import java.util.Properties;
+
+public class SSLTLSContextName {
+
+	public static final String JAVA_VENDOR_PROPERTY = "java.vendor";
+    public static final String JAVA_VERSION_PROPERTY = "java.version";
+    
+    
+    public static String getSelectedSSLContextName( Properties... props ) {
+        Properties p;
+        if( props.length == 0){
+            p = System.getProperties();
+        } else {
+            p = props[0];
+            }
+        boolean ibmJdk = p.getProperty(JAVA_VENDOR_PROPERTY).contains("IBM");
+        String name ;
+        if (ibmJdk) {
+            if (p.getProperty(JAVA_VERSION_PROPERTY).startsWith("8.")) {
+            	name="SSL_TLSv2"; // NOSONAR
+            }else {
+            	name ="TLSv1.2";
+            }
+        } else {
+            name = "TLSv1.2";
+        }
+        return name ;
+    }
+
+}
---
+++ galasa-managers-parent/galasa-managers-common-parent/dev.galasa.common/src/test/java/dev/galasa/SSLTLSContextNameTest.java
@@ -0,0 +1,68 @@
+package dev.galasa.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.* ;
+import java.util.Properties;
+import org.apache.commons.logging.impl.SimpleLog;
+
+public class SSLTLSContextNameTest {
+    @Test
+    public void testNonIBMJVMGivesTLS12Context() throws Exception {
+        new SimpleLog("Test log");
+        Properties testProps = new Properties();
+        testProps.setProperty(SSLTLSContextName.JAVA_VENDOR_PROPERTY,"FakeJavaRuntimeVendorName" );
+        // When...
+        String contextName = SSLTLSContextName.getSelectedSSLContextName(testProps);
+        // Then...
+        String expected = "TLSv1.2";
+        assertEquals(expected,contextName);
+    }
+    @Test
+    public void testIBMJVM8GivesSSL_TLSv2Context() throws Exception {
+        // Given...
+        Properties testProps = new Properties();
+        testProps.setProperty(SSLTLSContextName.JAVA_VENDOR_PROPERTY,"IBMJavaRuntimeVendorName" );
+        testProps.setProperty(SSLTLSContextName.JAVA_VERSION_PROPERTY, "8.0.16");
+        // When...
+        String contextName = SSLTLSContextName.getSelectedSSLContextName(testProps);
+        // Then...
+        String expected = "SSL_TLSv2";
+        assertEquals(expected,contextName);
+    }
+    
+    @Test
+    public void testIBMJVM11GivesTLSv12Context() throws Exception {
+        // Given...
+        Properties testProps = new Properties();
+        testProps.setProperty(SSLTLSContextName.JAVA_VENDOR_PROPERTY,"IBMJavaRuntimeVendorName" );
+        testProps.setProperty(SSLTLSContextName.JAVA_VERSION_PROPERTY, "11.0.16.1+1");
+        // When...
+        String contextName = SSLTLSContextName.getSelectedSSLContextName(testProps);
+        // Then...
+        String expected = "TLSv1.2";
+        assertEquals(expected,contextName);
+    }
+
+    @Test
+    public void testSystemPropertiesContextIsSelected() throws Exception {
+        // Given...
+        System.out.println(System.getProperty(SSLTLSContextName.JAVA_VENDOR_PROPERTY));
+        System.out.println(System.getProperty(SSLTLSContextName.JAVA_VERSION_PROPERTY));
+        // When...
+        String contextName = SSLTLSContextName.getSelectedSSLContextName();
+        // Then...
+        boolean ibmJdk = System.getProperty(SSLTLSContextName.JAVA_VENDOR_PROPERTY).contains("IBM");
+        String expected ;
+        if (ibmJdk) {
+            if (System.getProperty(SSLTLSContextName.JAVA_VERSION_PROPERTY).startsWith("8.")) {
+            	expected="SSL_TLSv2"; // NOSONAR
+            }else {
+            	expected ="TLSv1.2";
+            }
+        } else {
+            expected = "TLSv1.2";
+        }
+        assertEquals(expected,contextName);
+    }
+}
