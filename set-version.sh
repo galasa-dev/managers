2df85703420e66eb13f75023762e875be2c7336e
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2023-07-12 16:22:40 +0100

    set-version upgrades framework version dependency in buildSrc also
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- set-version.sh
+++ set-version.sh
@@ -18,6 +18,8 @@ export ORIGINAL_DIR=$(pwd)
 cd "${BASEDIR}/.."
 WORKSPACE_DIR=$(pwd)
 
+set -o pipefail
+
 
 #-----------------------------------------------------------------------------------------                   
 #
@@ -103,12 +105,43 @@ fi
 temp_dir=$BASEDIR/temp/version_bump
 mkdir -p $temp_dir
 
-# The galasa-parent/dev.galasa.framework/build.gradle file is where the 'master' version number 
-# of the framework component lives.
-# For example: version = "0.29.0"
-cat $BASEDIR/galasa-managers-parent/build.gradle | sed "s/^[ ]*version[ ]*=.*/version = \"$component_version\"/1" > $temp_dir/managers-build.gradle
+function upgrade_dependencies_on_framework {
+    h1 "Upgrading anything which references the framework component"
+    
+    source_path=${BASEDIR}/galasa-managers-parent/buildSrc/src/main/groovy/galasa.manager.gradle
+    temp_file=$temp_dir/dependency.gradle
+    # The line marked this:  implementation 'dev.galasa:dev.galasa.framework:0.27.0' needs to change.
+    info "Upgrading version framework in file $source_path"
 
-# Copy the temp files back to where they belong...
-cp $temp_dir/managers-build.gradle $BASEDIR/galasa-managers-parent/build.gradle
+    cat $source_path | sed "s/implementation[ \t]*.'dev.galasa:dev.galasa.framework:.*'[ \t]*$/implementation 'dev.galasa:dev.galasa.framework:$component_version'/1" > $temp_file
+    rc=$? ; if [[ "${rc}" != "0" ]]; then error "Failed to replace master version in file $source_path" ; exit 1 ; fi
+
+    cp $temp_file $source_path
+    rc=$? ; if [[ "${rc}" != "0" ]]; then error "Failed to replace master version file with the modified one." ; exit 1 ; fi
+
+    success "Upgraded dependencies on framework."
+}
+
+function upgrade_build_gradle {
+    h1 "Upgrading the component version number in the master gradle."
+
+    source_path=$BASEDIR/galasa-managers-parent/build.gradle
+    info "File to change is at file$source_path"
+
+    # The galasa-managers-parent/build.gradle file is where the 'master' version number 
+    # of the this component lives.
+    # For example: version = "0.29.0"
+    
+    cat $source_path | sed "s/^[ ]*version[ ]*=.*/version = \"$component_version\"/1" > $temp_dir/managers-build.gradle
+    rc=$? ; if [[ "${rc}" != "0" ]]; then error "Failed to replace master version in file $source_path" ; exit 1 ; fi
+
+    cp $temp_dir/managers-build.gradle $source_path
+    rc=$? ; if [[ "${rc}" != "0" ]]; then error "Failed to replace master version file with the modified one." ; exit 1 ; fi
+
+    success "Upgraded build.gradle file OK."
+}
+
+upgrade_build_gradle
+upgrade_dependencies_on_framework
 
 
